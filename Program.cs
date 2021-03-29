4fb74ba95c78fab629f3054d3a7664b3182db541
tom7now <65201171+tom7now@users.noreply.github.com>
2021-03-29 16:46:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal length;
+            decimal width;
+            decimal overage;
+            string input;
+
+            Console.Write("Enter the length of the room: ");
+            input = Console.ReadLine();
+
+            length = decimal.Parse(input);
+
+            Console.Write("Enter the width of the room: ");
+            input = Console.ReadLine();
+
+            width = decimal.Parse(input);
+
+            Console.Write("Enter the overage percent: ");
+            input = Console.ReadLine();
+
+            overage = decimal.Parse(input) / 100;
+
+            decimal area = length * width;
+
+            overage *= area;
+
+            area += overage;
+
+            Console.WriteLine($"The total area, including overage, is: {area}");
+        }
+    }
+}
